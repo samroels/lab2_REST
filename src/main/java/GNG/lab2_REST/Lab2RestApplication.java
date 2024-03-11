46131a6d2c6382edabf48a71a0d2c12aa9366bdc
Sam Roels <sam-roels@hotmail.com>
2024-03-11 09:53:07 +0100

    Initial commit

---
+++ src/main/java/GNG/lab2_REST/Lab2RestApplication.java
@@ -0,0 +1,13 @@
+package GNG.lab2_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GNG/lab2_REST/Lab2RestApplicationTests.java
@@ -0,0 +1,13 @@
+package GNG.lab2_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
